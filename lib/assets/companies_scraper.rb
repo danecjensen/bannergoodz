1ba5e8db91cef8eda30265b7b931655d92a3f668
Lukas Nimmo <Lukas.nimmo@gmail.com>
2017-02-28 12:00:04 -0600

    Can now find correct social media links for multiple word names

--- lib/assets/companies_scraper.rb
+++ lib/assets/companies_scraper.rb
@@ -4,6 +4,27 @@ require 'open-uri'
 require 'selenium-webdriver'
 require 'pry'
 
+
+# Feel free to do as much or as little as you want on this project.  I saw you did some web scraping and thought this would be a fun project for you to try out.  We will talk about it briefly on Thursday.  
+# 
+#   I made a repo as a starting point: https://github.com/danecjensen/bannergoodz.  If you clone that repo there’s a csv file in the main directory called companylist.csv.  For each website in that csv I want you to pull the following information:
+# 
+#   <meta name="description" content="" />
+# <meta property="og:title" content=”">
+# <meta property="og:image" content="" />
+# (a profile picture from facebook page or instagram page that corresponds to the website)
+# 
+# Then insert that information in the Rails model Brand (https://github.com/danecjensen/bannergoodz/blob/master/app/models/brand.rb).
+# 
+#   Specifically (where b = Brand.new):
+#   <meta name="description" content="" /> ----> b.desc
+# <meta property="og:title" content=""> ----> b.name
+# <meta property="og:image" content="" /> ----> b.card_image (this is an url string)
+# (a profile picture from facebook page or instagram page that corresponds to the website) ----> b.profile_image (this is an url string)
+
+
+
+
 # For now, scrapes website information based on the following root-level file:
 # companylist.csv
 class CompaniesScraper
@@ -16,7 +37,7 @@ class CompaniesScraper
   def scrape
     CSV.foreach(@source, headers: true) do |row|
       html = @request_handler.page_info(row['URL'])
-      @co_parser.parse(html, row['URL']) if html
+      @co_parser.parse(html, row['URL'], row['Anchor']) if html
     end
   end
 
@@ -31,13 +52,14 @@ class CompaniesParser
   META_DESCRIPTION = "meta[name='description']".freeze
   META_TITLE = "meta[property='og:title']".freeze
   META_IMAGE = "meta[property='og:image']".freeze
-  FACEBOOK_SELECTOR = 'a[href^="https://www.facebook"]'.freeze
-  INSTAGRAM_SELECTOR = 'a[href^="https://www.instagram"]'.freeze
+  FACEBOOK_SELECTOR = 'a[href*="//www.facebook"]'.freeze
+  INSTAGRAM_SELECTOR = 'a[href*="//www.instagram"]'.freeze
+  SPECIAL_CHARACTERS = ['&']
 
-  def parse(html, url)
+  def parse(html, url, name)
     @html = html
     @url = url
-    @name = url.match(/http[s]?:\/\/[w]{0,3}\.?(.*)\./)[1]
+    @name = cleanse_name(name)
     @meta_info = {
       description: '',
       title:       '',
@@ -56,6 +78,24 @@ class CompaniesParser
 
   private
 
+  def cleanse_name(name)
+    name.strip.split(' ').delete_if do |n|
+      result = false
+      SPECIAL_CHARACTERS.each do |c|
+        if c == n
+          result = true
+          break
+        end
+      end
+    end
+  end
+
+  def css_name_selector
+    @name.reduce('') do |sum, n|
+      sum + "[href*='#{n}']"
+    end
+  end
+
   def set_meta_info
     @meta_info.each do |k, _|
       data = @html.css(self.class.const_get("META_#{k.to_s.upcase}"))[0]
@@ -66,7 +106,7 @@ class CompaniesParser
 
   def set_social_links
     @social_links.each do |k, _|
-      data = @html.css(self.class.const_get("#{k.to_s.upcase}_SELECTOR") + "[href*='#{@name}']")[0]
+      data = @html.css(self.class.const_get("#{k.to_s.upcase}_SELECTOR") + css_name_selector)[0]
       @social_links[k] = data['href'] if data
       p @social_links
     end
@@ -92,7 +132,7 @@ class CompaniesParser
     if @social_links.values.keep_if { |s| !s.empty? }.empty?
       @social_links.each do |k,_|
         begin
-          el = driver.find_element(:css, self.class.const_get("#{k.to_s.upcase}_SELECTOR") + "[href*='#{@name}']") # Need to account for multiple word names, need to also get rid of special characters
+          el = driver.find_element(:css, self.class.const_get("#{k.to_s.upcase}_SELECTOR") + css_name_selector) # Need to account for multiple word names, need to also get rid of special characters
           @social_links[k] = el['href']
         rescue Selenium::WebDriver::Error::NoSuchElementError
           puts "No #{k} element found"
@@ -118,6 +158,8 @@ class CompaniesParser
       @profile_picture = el['src'] if el
       p @profile_picture
     end
+
+    driver.quit
   end
 end
 
